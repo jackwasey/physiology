239ed02176faec65a1d1aa2a37c4e8d0a9d85a04
Jack Wasey <jack@jackwasey.com>
2016-06-21 22:06:02 -0400

    big A in alveolar gas equation PAO2

--- R/pulmonary.R
+++ R/pulmonary.R
@@ -1,6 +1,6 @@
 #' alveolar gas equation
 #'
-#' Estimate paO2 in alveolus based on atmospheric pressure, FiO2, PACO2, and
+#' Estimate PAO2 in alveolus based on atmospheric pressure, FiO2, PACO2, and
 #' respiratory quotient
 #' @param FiO2 fraction of oxygen in inspired gas, from 0 to 1, default is (dry)
 #'   room air
@@ -15,15 +15,15 @@
 #' @examples
 #' # vary RQ
 #' rq <- seq(0.6, 1.4, 0.05)
-#' plot(rq, alveolar_paO2_mmHg(RQ = rq))
+#' plot(rq, alveolar_PAO2_mmHg(RQ = rq))
 #'
 #' # 100% FiO2 at typical atmospheric pressure
-#' alveolar_paO2_mmHg(FiO2 = 1)
+#' alveolar_PAO2_mmHg(FiO2 = 1)
 #'
 #' # hyperbaric oxygen at 100%, 2 atmospheres
-#' alveolar_paO2_mmHg(FiO2 = 1, Patm_mmHg = 1520)
+#' alveolar_PAO2_mmHg(FiO2 = 1, Patm_mmHg = 1520)
 #' @export
-alveolar_PaO2_mmHg <- function(FiO2 = 0.209, RQ = 0.8, PACO2_mmHg = 40, Patm_mmHg = 760, PAH2O_mmHg = 47) {
+alveolar_PAO2_mmHg <- function(FiO2 = 0.209, RQ = 0.8, PACO2_mmHg = 40, Patm_mmHg = 760, PAH2O_mmHg = 47) {
   stopifnot(FiO2 >= 0, FiO2 <= 1)
   stopifnot(RQ > 0, RQ < 10)
   stopifnot(PACO2_mmHg > 0, PACO2_mmHg < (Patm_mmHg - PAH2O_mmHg))
