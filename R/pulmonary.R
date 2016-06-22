9ffdcf03fcf03eabd0481c0355095c4aec93be3a
Jack Wasey <jack@jackwasey.com>
2016-06-21 21:52:38 -0400

    test initial pulmonary and fix notation

--- R/pulmonary.R
+++ R/pulmonary.R
@@ -1,16 +1,16 @@
 #' alveolar gas equation
 #'
-#' Estimate paO2 in alveolus based on atmospheric pressure, FiO2, PaCO2, and
+#' Estimate paO2 in alveolus based on atmospheric pressure, FiO2, PACO2, and
 #' respiratory quotient
 #' @param FiO2 fraction of oxygen in inspired gas, from 0 to 1, default is (dry)
 #'   room air
 #' @param RQ respiratory quotient, i.e. the ratio of CO2 produced to oxygen
 #'   consumed, usually between around 0.7 and 1.0, but can legitimately be
 #'   greater than 1.0. Default it 0.8.
-#' @param paCO2_mmHg partial pressure of CO2 in alveolus, which can be approximated
+#' @param PACO2_mmHg partial pressure of CO2 in alveolus, which can be approximated
 #'   as the end-tidal pCO2
 #' @param Patm_mmHg atmospheric pressure in kPa
-#' @param paH2O_mmHg partial pressure of water vapor, defaults to 6.25 kPa (47 mmHg)
+#' @param PAH2O_mmHg partial pressure of water vapor, defaults to 6.25 kPa (47 mmHg)
 #'   which is appropriate for body temperature
 #' @examples
 #' # vary RQ
@@ -23,12 +23,12 @@
 #' # hyperbaric oxygen at 100%, 2 atmospheres
 #' alveolar_paO2_mmHg(FiO2 = 1, Patm_mmHg = 1520)
 #' @export
-alveolar_paO2_mmHg <- function(FiO2 = 0.209, RQ = 0.8, paCO2_mmHg = 40, Patm_mmHg = 760, paH2O_mmHg = 47) {
+alveolar_PaO2_mmHg <- function(FiO2 = 0.209, RQ = 0.8, PACO2_mmHg = 40, Patm_mmHg = 760, PAH2O_mmHg = 47) {
   stopifnot(FiO2 >= 0, FiO2 <= 1)
   stopifnot(RQ > 0, RQ < 10)
-  stopifnot(paCO2_mmHg > 0, paCO2_mmHg < (Patm_mmHg - paH2O_mmHg))
+  stopifnot(PACO2_mmHg > 0, PACO2_mmHg < (Patm_mmHg - PAH2O_mmHg))
   stopifnot(Patm_mmHg > 0)
-  stopifnot(paH2O_mmHg > 0, paH2O_mmHg < (Patm_mmHg - paCO2_mmHg))
+  stopifnot(PAH2O_mmHg > 0, PAH2O_mmHg < (Patm_mmHg - PACO2_mmHg))
 
-  FiO2 * (Patm_mmHg - paH2O_mmHg) - paCO2_mmHg * (1 - FiO2 * (1 - RQ)) / RQ
+  FiO2 * (Patm_mmHg - PAH2O_mmHg) - PACO2_mmHg * (1 - FiO2 * (1 - RQ)) / RQ
 }
---
+++ tests/testthat/test-pulmonary.R
@@ -0,0 +1,14 @@
+context("acid base")
+
+test_that("bad data throws errors in alveolar gas eqtn", {
+  expect_error(alveolar_PaO2_mmHg(FiO2 = -1))
+  expect_error(alveolar_PaO2_mmHg(FiO2 = 1.1))
+  expect_error(alveolar_PaO2_mmHg(RQ = -1))
+  expect_error(alveolar_PaO2_mmHg(RQ = 99))
+  expect_error(alveolar_PaO2_mmHg(PACO2_mmHg = -1))
+  expect_error(alveolar_PaO2_mmHg(PACO2_mmHg = 50000)) # greater than atmospheric
+  expect_error(alveolar_PaO2_mmHg(Patm_mmHg = -1))
+  expect_error(alveolar_PaO2_mmHg(Patm_mmHg = 0.1)) # less than other gasses
+  expect_error(alveolar_PaO2_mmHg(PAH2O_mmHg = -1))
+  expect_error(alveolar_PaO2_mmHg(PAH2O_mmHg = 1000)) # more than atmospheric
+})
