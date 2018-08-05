82acec464cca5389dbafa1f6d76b43970ba7021f
Jack O. Wasey <jack@jackwasey.com>
2018-08-05 00:00:16 -0400

    renamed deadspace to airway, and ventilation to deadspace, documented, examples

---
+++ R/airway.R
@@ -0,0 +1,71 @@
+#' Estimate volume inside an endotracheal tube
+#'
+#' Calculations are based on standard endotracheal tubes. The volume is
+#' estimated as the cylinder of the given diameter and typical length of a tube
+#' of given size.
+#' @param diameter_mm The internal diameter of the endotracheal tube in
+#'   millimeters. Sizes between 2.0 and 8 are offered. Half sizes between 2.5
+#'   and 6 are accepted.
+#' @examples
+#' ett_vol_ml(2:8)
+#' plot(2:8, ett_vol_ml(2:8))
+#' lines(2:8, ett_vol_ml(2:8),
+#'   xlab = "ETT internal diameter, mm",
+#'   ylab = "ETT internal volume, mm^3")
+#' (vols_cm3 <- ett_vol_ml(seq(2, 6, 0.5)) / 1000)
+#'
+#' # Ages through to ETT internal volume
+#' ett_vol_ml(ett_size_by_age(ages))
+#'
+#' @return Volumes of each given ETT in cubic millimeters
+#' @export
+ett_vol_ml <- function(diameter_mm) {
+  lookup_diam_mm <- c(2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 7, 8)
+  lookup_len_cm <- c(14, 15, 17, 19, 21, 23, 25, 28, 29, 31, 33)
+  stopifnot(diameter_mm %in% lookup_diam_mm)
+  len_mm <- 10 * lookup_len_cm[match(diameter_mm, lookup_diam_mm)]
+  (len_mm * pi * (diameter_mm / 2) ^ 2) / 1000
+}
+#' Estimate appropriate size of endotracheal tube for infants and children
+#'
+#' \code{ett_size_cole} uses the classic Cole formula for uncuffed tubes,
+#' Motoyama formula for cuffed tubes with age over two years, and the Khine
+#' formula for cuffed tubes with age under two years. All of these, as any
+#' anesthesiologist will tell, give poor estimates at any extreme of age, or
+#' height. These formulae are for use in pedatric practice only.
+#' @param age_y numeric vector
+#' @param cuffed logical vector, single value, or a vector of the same length as
+#'   the given age vector, defining whether the desired tube is to be cuffed. If
+#'   not specified, then it is assumed that a cuffed tube is used.
+#' @return ETT size, internal diameter in mm, rounded to nearest half-mm size up
+#'   to 6mm, then the nearest integer.
+#' @examples
+#' teenagers <- ett_size_by_age(13:18)
+#' plot(13:18, teenagers,
+#'   main = "This formula for ETT tube size overestimates tube
+#'   sizes of teenagers, and takes no account of gender")
+#' lines(13:18, teenagers)
+#' ages <- c(1/12, 1, 2, 4, 7, 11)
+#' neonate_to_child <- ett_size_by_age(age = ages)
+#' names(neonate_to_child) <- ages
+#' print(neonate_to_child)
+#' plot(ages, neonate_to_child)
+#' lines(ages, neonate_to_child)
+#' plot(ages, neonate_to_child, log = "x")
+#' lines(ages, neonate_to_child)
+#' @references
+#' http://anesthesiology.pubs.asahq.org/Article.aspx?articleid=1933172
+#' @export
+ett_size_by_age <- function(age_y, cuffed = TRUE) {
+  cole <- 4 + age_y / 4
+  motoyama <- 3.5 + age_y / 4
+  khine <- 3 + age_y / 4
+  out <- cole
+  if (!is.null(cuffed)) {
+    out[cuffed & age_y >= 2] <- motoyama[cuffed & age_y >= 2]
+    out[cuffed & age_y < 2] <- khine[cuffed & age_y < 2]
+  }
+  out[out < 6] <- round(out[out < 6] * 2) / 2
+  out[out >= 6] <- round(out[out >= 6])
+  out
+}
--- R/deadspace.R
+++ R/deadspace.R
@@ -1,21 +1,81 @@
-#' Estimate volume inside an endotracheal tube
+#' Estimate ventilation dead-space
 #'
-#' Calculations are based on standard endotracheal tubes. The volume is
-#' estimated as the cylinder of the given diameter and typical length of a tube
-#' of given size.
-#' @param diameter_mm The internal diameter of the endotracheal tube in
-#'   millimeters. Sizes between 2.0 and 8 are offered. Half sizes between 2.5
-#'   and 6 are accepted.
+#' @param ideal_weight_kg Ideal weight in kilograms. May be calculated using
+#'   \code{\link{ideal_weight_adult}} or \code{\link{ideal_weight_child}}
+#' @param age_y Age in years, optional for estimating ETT and HME sizes
+#'   automatically
+#' @param elbow_ml Numeric volume of elbow of breathing circuit in ml
+#' @param humidifier_ml Numeric volume of humidifier of breathing circuit in ml
+#' @param ett_diameter_mm Numeric internal diameter of endotracheal tube.
+#'   Default is \code{NULL} which would estimate this from the age of patient
 #' @examples
-#' ett_vol_mm3(2:8)
-#' plot(ett_vol_mm3(2:8))
-#' lines(ett_vol_mm3(2:8))
-#' ett_vol_mm3(seq(2, 6, 0.5))
-#' @return Volumes of each given ETT in cubic millimeters
-ett_vol_mm3 <- function(diameter_mm) {
-  lookup_diam_mm <- c(2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 7, 8)
-  lookup_len_cm <- c(14, 15, 17, 19, 21, 23, 25, 28, 29, 31, 33)
-  stopifnot(diameter_mm %in% lookup_diam_mm)
-  len_mm <- 10 * lookup_len_cm[match(diameter_mm, lookup_diam_mm)]
-  len_mm * pi * (diameter_mm/2)^2
+#'   height <- seq(1, 2, 0.05)
+#'   male <- rep(FALSE, length(height))
+#'   iw <- ideal_weight_adult(height_m = height, male = male)
+#'   plot(iw, deadspace_anatomic_adult(ideal_weight_kg = height))
+#'
+#'   # discontinuity at age 6 is driven by ideal weight more than the
+#'   # lograithmic calculation
+#'   iw <- c(seq(12, 18, 0.2), seq(18.5, 24, 0.5))
+#'   youngest = 3
+#'   oldest = 9
+#'   ages <- seq(youngest, oldest, (oldest - youngest) / (length(iw) - 1))
+#'   plot(iw, deadspace_anatomic_child(ideal_weight_kg = iw, age_y = ages),
+#'        type = "l")
+#'
+#' @export
+deadspace_total <- function(ideal_weight_kg,
+                            age_y = NULL,
+                            elbow_ml = 10,
+                            humidifier_ml = 7,
+                            ett_diameter_mm = NULL) {
+
+}
+
+#' @describeIn deadspace_total Estimate anatomic dead-space
+#' @references
+#'   \url{http://www.atsjournals.org/doi/abs/10.1164/arrd.1971.104.2.215}
+#'   \url{http://rc.rcjournal.com/content/53/7/885.short}
+#'   \url{https://www.ncbi.nlm.nih.gov/pubmed/8727530}
+#' @return estimate of anatomic dead-space in ml
+#' @export
+deadspace_anatomic <- function(ideal_weight_kg, age_y = NULL) {
+  if (is.null(age_y) || age_y >= 18)
+    deadspace_anatomic_adult(ideal_weight_kg = ideal_weight_kg)
+  else
+    deadspace_anatomic_child(ideal_weight_kg = ideal_weight_kg, age_y = age_y)
+}
+
+#' @describeIn deadspace_total Estimate anatomic dead-space in an adult
+#' @export
+deadspace_anatomic_adult <- function(ideal_weight_kg = NULL) {
+  if (is.null(ideal_weight_kg))
+    125
+  else
+    2.2 * ideal_weight_kg
+}
+
+#' @describeIn deadspace_total Estimate anatomic dead-space in an infant or
+#'   child
+#' @export
+deadspace_anatomic_child <- function(ideal_weight_kg, age_y = NULL) {
+  if (is.null(age_y) || age_y >= 6)
+    deadspace_anatomic_adult(ideal_weight_kg = ideal_weight_kg)
+  else
+    ideal_weight_kg * (3.28 - 0.56 * log(1 + age_y))
+}
+
+#' @describeIn deadspace_total intrathoracic component of dead-space is age
+#'   independent
+#' @details 'Mean intrathoracic anatomic dead space was 1.03 ml/kg and was not
+#'   related to age.' Numa, 1985
+deadspace_intrathoracic_ml <- function(ideal_weight_kg)
+  1.03 * ideal_weight_kg
+
+deadspace_extrathoracic <- function(ideal_weight_kg = NULL, age_y = NULL) {
+.NotYetImplemented()
+}
+
+deadspace_physiologic <- function(ideal_weight_kg) {
+  .NotYetImplemented()
 }
